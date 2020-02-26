5b30c10ec0698e5e4aa8719b8c05202f65f84fb3
Luke <lmschless@gmail.com>
2020-02-25 16:27:56 -0800

    updated triangle.js to use class syntax

--- src/triangle.js
+++ src/triangle.js
@@ -1,23 +1,25 @@
-export function Triangle(side1, side2, side3) {
-	this.side1 = side1;
-	this.side2 = side2;
-	this.side3 = side3;
-}
-
-Triangle.prototype.checkType = function() {
-	if (
-		this.side1 > this.side2 + this.side3 ||
-		this.side2 > this.side1 + this.side3 ||
-		this.side3 > this.side1 + this.side2
-	) {
-		return 'not a triangle';
-	} else if (this.side1 === this.side2 && this.side2 === this.side3) {
-		return 'it is an equilateral triangle';
-	} else if (this.side1 === this.side2 || this.side1 === this.side3 || this.side2 === this.side3) {
-		return 'it is an isosceles triangle';
-	} else if (this.side1 !== this.side2 && this.side1 !== this.side3 && this.side2 !== this.side3) {
-		return 'it is a scalene triangle';
-	} else {
-		return 'whatt';
+export class Triangle {
+	constructor(side1, side2, side3) {
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
 	}
-};
+
+	checkType() {
+		if (
+			this.side1 > this.side2 + this.side3 ||
+			this.side2 > this.side1 + this.side3 ||
+			this.side3 > this.side1 + this.side2
+		) {
+			return 'not a triangle';
+		} else if (this.side1 === this.side2 && this.side2 === this.side3) {
+			return 'it is an equilateral triangle';
+		} else if (this.side1 === this.side2 || this.side1 === this.side3 || this.side2 === this.side3) {
+			return 'it is an isosceles triangle';
+		} else if (this.side1 !== this.side2 && this.side1 !== this.side3 && this.side2 !== this.side3) {
+			return 'it is a scalene triangle';
+		} else {
+			return 'whatt';
+		}
+	}
+}
